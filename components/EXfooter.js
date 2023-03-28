f96fabe0ac3391b45e883833459bf9caa237e92d
AYAAN ALAM <77435615+ayaan07alam@users.noreply.github.com>
2023-03-28 15:53:55 +0530

    Add files via upload

---
+++ components/EXfooter.js
@@ -0,0 +1,114 @@
+function footer(){
+  return ` <footer>
+  <div id="footer">
+    <div id="mainfoot">
+      <div id="foot1">
+        <div id="footimg">
+          <img src="imgaes/logo.jpeg" alt="">
+        </div>
+        <div id="footlogo">
+          <a href="#"> <i class="fa-brands fa-facebook-f"></i></a>
+          <a href="#"> <i class="fa-brands fa-twitter"></i></a>
+          <a href="#"> <i class="fa-brands fa-linkedin"></i></a>
+          <a href="#"> <i class="fa-brands fa-youtube"></i></a>
+          <a href="#"> <i class="fa-brands fa-instagram"></i></a>
+          <a href="#"> <i class="fa-brands fa-whatsapp"></i></a>
+
+
+        </div>
+
+        <div id="follow">
+          <h1> 1.5M +</h1>
+          <p>Followers</p>
+        </div>
+
+        <div id="Footcontact">
+          <p>For any queries</p>
+          <p>Email: info@coinDCF.org</p>
+          <p> Contact No: +91 7989532458</p>
+
+        </div>
+      </div>
+      <div id="foot2">
+        <h2>Cause</h2>
+        <div>
+          <p>Medical crowdfunding</p>
+          <p>Cancer Crowdfunding</p>
+          <p>Education</p>
+          <p>Crowdfunding</p>
+          <p>Creative Fundraising</p>
+          <p>Child Welfare</p>
+          <p>Animal Fundraisers</p>
+          <p>COVID-19 Relief Fund</p>
+
+        </div>
+      </div>
+      <div id="foot3">
+        <h2>How it works?</h2>
+        <div>
+          <p>Fundraising for NGOs</p>
+          <p>Sponsor A Child</p>
+          <p>Fundraising Tips</p>
+          <p>What is Crowdfunding?</p>
+          <p>Corporates</p>
+          <p>Withdraw Funds</p>
+          <p>Browse Fundraiser</p>
+
+
+        </div>
+      </div>
+      <div id="foot4">
+        <h2>About Us</h2>
+        <div>
+          <p>Team COINCDF</p>
+          <p>In The News</p>
+          <p>Our Partners</p>
+          <p>Careers</p>
+          <p>Ketto Blog</p>
+          <p>Success Stories</p>
+
+
+
+        </div>
+      </div>
+      <div id="foot5">
+        <h2>Support</h2>
+        <div>
+          <p>Medical Finance</p>
+          <p>FAQs & Help Center</p>
+          <p>Are COINCDF Campaigns</p>
+          <p>Genuine?</p>
+          <p>Fundraiser Video</p>
+          <p>Trust & Safety</p>
+          <p>Plans & Pricing</p>
+          <p>Contact Us</p>
+
+        </div>
+      </div>
+      
+    </div>
+    
+    <div id="bottomfoot">
+    
+        <img src="	https://ketto.gumlet.io/assets/images/homepage/footer-secured-card.png?w=457&dpr=2.0" alt="">
+     
+
+    </div>
+    </div>
+
+    <div id="affooter">
+      <p>Copyright © 2023 COINCDF. All Rights Reserved.
+        Terms of Use |
+        Privacy Policy |
+        AML Policy |
+        Use of cookies </p>
+    </div>
+
+    <div id="affooter2">
+      <p>Know more about COINCDF Platform</p>
+    </div>
+
+
+  </footer>`
+}
+export default footer; 
